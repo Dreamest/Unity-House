dbd6e19132744e591a304b8c835e862280ec9d26
Dreamest <steels001@gmail.com>
2020-08-12 19:58:20 +0300

    Added Door Movement

---
+++ Unity-House/Assets/Scripts/DoorMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMovement : MonoBehaviour
+{
+    private Animator animator;
+    private bool isOpen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isOpen = false;
+        Debug.Log("Start");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Y");
+        if (other.CompareTag("MainCamera"))
+        {
+            animator.SetTrigger("Open");
+            isOpen = true;
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("X");
+        if (isOpen)
+        {
+            animator.SetTrigger("Close");
+            isOpen = false;
+        }
+    }
+}
